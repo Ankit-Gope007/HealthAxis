641402c76c84da5a52a685cf466465235bac95fd
Ankit Gope <ankitgope21@gmail.com>
2025-05-20 17:49:29 +0530

    Working on Profile page

--- src/app/patient/(dashboard)/profile/page.tsx
+++ src/app/patient/(dashboard)/profile/page.tsx
@@ -1,14 +1,53 @@
+"use client"
 import React from 'react'
+import { useRef } from 'react';
 import { FaRegUser } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+import { CiCalendarDate } from "react-icons/ci";
+import { MdOutlineWaterDrop } from "react-icons/md";
+import { IoCallOutline } from "react-icons/io5";
+
 import './page.css';
 
 const page = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   return (
     <div className='patient-profile-container'>
       <div className="profile-summary">
         <div className="profile-info">
+          <div className="profile-info-header">
+            <h1>Patient Profile</h1>
+            <p>Manage your health information</p>
+          </div>
+          <div
+            onClick={() => fileInputRef.current?.click()}
+           className="profile-info-photo">
+              <IoMdAdd className='add-icon' />
+              <input 
+                ref={fileInputRef}
+               type="file"
+                id="file-upload"
+                 />
+          </div>
+          <div className="profile-info-names">
+            <h2 className=''>John Doe</h2>
+            <p>Patient ID: 123456HD-UKL</p>
+          </div>
+          <div className="profile-info-basicInfo">
+            <div className="profile-info-basicInfo-age">
+              <p>Age: 30 years</p>
+            </div>
+            <div className="profile-info-basicInfo-blood">
 
+            </div>
+            <div className="profile-info-basicInfo-contact">
+
+            </div>
+          </div>
         </div>
+
+
+
         <div className="profile-data">
 
         </div>
@@ -25,7 +64,7 @@ const page = () => {
           <div className="info">
             <form action="" className='form-grid'>
               <div className="form-group">
-                <label><FaRegUser className='green'/>  Full Name*</label>
+                <label><FaRegUser className='green' />  Full Name*</label>
                 <input type="text" placeholder="John Doe" />
               </div>
               <div className="form-group">
