fb9f25f2914760ba9558c7cd0ec169a7c663f298
Ankit Gope <ankitgope21@gmail.com>
2025-08-02 13:38:40 +0530

    Update useSocket.ts

--- src/lib/useSocket.ts
+++ src/lib/useSocket.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import io, { Socket as SocketType } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 let socket: SocketType | null = null;
 
